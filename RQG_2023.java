7702a94573ad830dd40020c2078f6b6312fbe97d
ramontreca <125561580+ramontreca@users.noreply.github.com>
2023-02-25 17:24:54 +0100

    Create RQG_2023.java

---
+++ RQG_2023.java
@@ -0,0 +1,12 @@
+package proyecto_RQG_20230225;
+
+public class RQG_2023 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola mundo");
+	}
+
+}
